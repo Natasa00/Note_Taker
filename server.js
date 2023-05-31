871ebaf424c9fb1dac05f15d3f4e9848fff72ee2
Natasa <nkilibarda3@gmail.com>
2023-05-31 18:25:32 +1000

    fix serving static files from server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const { uuid } = require("uuidv4");
 
 const app = express();
@@ -9,9 +10,10 @@ const port = 3000;
 app.use(express.json());
 
 app.get("/api/notes", function (req, res, next) {
+  console.log("getting notes api");
   try {
     // read the db
-    const data = fs.readFileSync("./api/db/db.json", "utf8");
+    const data = fs.readFileSync("./db/db.json", "utf8");
     // return all notes
     res.send(data);
   } catch (err) {
@@ -27,18 +29,24 @@ app.post("/api/notes", function (req, res, next) {
   };
 
   try {
-    const data = fs.readFileSync("./api/db/db.json", "utf8");
+    const data = fs.readFileSync("./db/db.json", "utf8");
     const existingNotes = JSON.parse(data);
     existingNotes.push(newNote);
-    fs.writeFileSync(
-      "./api/db/db.json",
-      JSON.stringify(existingNotes, null, 2)
-    );
+    fs.writeFileSync("./db/db.json", JSON.stringify(existingNotes, null, 2));
     res.send(newNote);
   } catch (err) {
     next(err);
   }
 });
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"))
+})
+
+app.use("/notes", express.static(__dirname + "/public/notes.html"));
+
 
 app.listen(port, () => {
   console.log(`Notes API listening on port ${port}`);
